8058fd25e7c938f826e48a5fc2f64c7d5930292b
Thierry Jousson <thierryjousson@live.fr>
2024-11-12 16:43:41 +0100

    Premiere version du jeux du Simon

--- EpreuveCodeTrois.py
+++ EpreuveCodeTrois.py
@@ -1,79 +1,56 @@
 # region Imports
-
-
-
+import os
+import random
+import time
 # endregion
 
 
 # region Variables globales
-
 # endregion
 
 
 # region Fonctions
-# Fonction d'affichge du menu du programme, retourne le choix de l'utilisateur
-def afficher_menu():
-    print("**********************************************")
-    print("*        Programme de ...                *")
-    print("**********************************************")
-    print("*     1 - xxx                                *")
-    print("*     2 - xxx                                *")
-    print("*     3 - xxx                                *")
-    print("*     4 - Quitter                            *")
-    print("********************************************\n")
-    return input("Choisissez votre .... : ")
-
-
-# Fonction de saisie de réponse utilisateur pour continuer dans la séquence de conversion choisi ou de revenir au menu
-def demander_pour_continuer():
-    r = str(input("Voulez-vous continuer : o/n ?").lower())
-    if r == "o":
-        return True
-    elif r == "n":
-        return False
+# Fonction d'éffacement du terminal console
+def clear_screen():
+    if(os.name == 'posix'):
+        os.system('clear')
     else:
-        print("Je n'ai pas compris votre saisie")
-        return demander_pour_continuer()
+        os.system('cls')
 
-
-
-
-
-
-
-
+# Fonction de génération d'un chiffre aléatoire avec x nombres
+def ajout_nombre_aleatoire(seq, ite):
+    for i in range(ite):
+        seq += str(random.randint(0,9))
+    return seq
 # endregion
 
 # region Programme principale
+# Lancement du programme principal
 def LancementProgramme():
-    # boucle du menu
+    sequence = ""
+    sequence = ajout_nombre_aleatoire(sequence, 3)
+    score = 0
+
+    # Boucle itérative des séquences de nombres
     while True:
-        choix = afficher_menu()
-        if choix == "4":
+        print("Retenez la séquence")
+        time.sleep(1)
+        sequence = ajout_nombre_aleatoire(sequence, 1)
+        print(sequence)
+        time.sleep(3)
+        clear_screen()
+        reponse = input("Votre réponse : ")
+        if(reponse == sequence):
+            score += 1
+            print("Bonne réponse")
+            print(f"Votre score est : {score}")
+            time.sleep(1)
+            clear_screen()
+        else :
+            print(f"Mauvaise réponse, la séquence était {sequence}")
+            print(f"Votre score final : {score}")
             break
-
-        # boucle de conversion selon le mode choisi
-        while True:
-            if choix == "1":
-
-                # demande de sortie du mode de conversion
-                if demander_pour_continuer() == False:
-                    break
-            elif choix == "2":
-
-                # demande de sortie du mode de conversion
-                if demander_pour_continuer() == False:
-                    break
-            elif choix == "3":
-
-                # demande de sortie du mode de conversion
-                if demander_pour_continuer() == False:
-                    break
-            else:
-                print("ERREUR : Veuillez saisir 1,2,3 ou 4 pour sortir.")
-
     # sortie du programme
     print("Fin du programme")
     exit()
-
 # endregion
--- EpreuveCodeUn.py
+++ EpreuveCodeUn.py
@@ -4,46 +4,83 @@
 
 
 # region Variables globales
-
+# 1 pouce = 2.54 cm
+# 1 cm = 0.394 pouces
+POUCE_POUR_CM = 2.54
+CM_POUR_POUCE = 0.394
 # endregion
 
 
 # region Fonctions
+# Fonction d'affichge du menu du programme, retourne le choix de l'utilisateur
+def afficher_menu():
+    print("*****************************************")
+    print("*        Programme de conversion        *")
+    print("*****************************************")
+    print("*     1 - Convertion pouces -> cm       *")
+    print("*     2 - Convertion cm -> pouces       *")
+    print("*     3 - Quitter                       *")
+    print("*****************************************\n")
+    return input("Choisissez une conversion : ")
 
+
+# Fonction de demande de saisie de la valeur à convertir et de convertion
+def demander_convertir_valeur(unite_un, unite_deux, valeur_de_conversion):
+    valeur = input(f"Saisissez la valeur à convertir :")
+    try:
+        valeur_float = float(valeur)
+    except:
+        print("ERREUR : Vous devez rentrer une valeur numérique")
+        print("(Utilisez le point et non la virgule pour les décimales)")
+    # Affichge de la valeur de conversion
+    valeur_convertie = round(valeur_float * valeur_de_conversion, 2)
+    print(f"Résultat de la conversion : {valeur_float} {unite_un} = {valeur_convertie} {unite_deux}\n")
+
+
+# Fonction de saisie de réponse utilisateur pour continuer dans la séquence de conversion choisi ou de revenir au menu
+def demander_pour_continuer():
+    r = str(input("Voulez-vous continuer : o/n ?").lower())
+    if r == "o":
+        return True
+    elif r == "n":
+        return False
+    else:
+        print("Je n'ai pas compris votre saisie")
+        return demander_pour_continuer()
 # endregion
 
-# 1 pouce = 2.54 cm
-# 1 cm = 0.394 pouces
 
+"""
+Programme principale de l'application
+Ce programme à pour but de convertir des valeur de pouce en cm ou inversement 
+Le programme demandera un choix utilisateur sur le mode de converion et lancera un module de conversion à la chaine
+L'utilisateur pourra interrrompre cette sépquence et revenir au menu principal
+Le menu permmtra de mettre fin au programme sur un choix utilisateur
+"""
 # region Programme principale
 def LancementProgramme():
-    print("Début du programme")
-    print()
-    print("Menu")
-    print("1 - Conversion pouces vers cm")
-    print("2 - Conversion cm vers pouces")
-    print("X - Sortir")
-    print()
-
-    sortir = False
-    while sortir != True:
-        choix_str = input("Choisissez votre conversion (saisissez 1 ou 2) : ")
-        if choix_str.lower() == "x":
-            sortir = True
+    # boucle du menu
+    while True:
+        choix = afficher_menu()
+        if choix == "3":
             break
-        elif int(choix_str) == 1:
-            print("Conversion de pouces vers cm")
-            valeur_str = input("Saisissez la valeur à convertir : ")
-            resultat = float(valeur_str)*2.54
-            print(f"{valeur_str} pouces sont égales à : {round(resultat,2)} cm")
-        elif int(choix_str) == 2:
-            print("Conversion de cm vers pouces")
-            valeur_str = input("Saisissez la valeur à convertir : ")
-            resultat = float(valeur_str) *0.394
-            print(f"{valeur_str} cm sont égales à : {int(resultat)} pouces")
-        else:
-            print("ERREUR : Veuillez saisir 1 ou 2 ou X pour sortir.")
 
+        # boucle de conversion selon le mode choisi
+        while True:
+            if choix == "1":
+                demander_convertir_valeur("pouces", "cm", POUCE_POUR_CM)
+                # demande de sortie du mode de conversion
+                if demander_pour_continuer() == False:
+                    break
+            elif choix == "2":
+                demander_convertir_valeur("cm", "pouces", CM_POUR_POUCE)
+                # demande de sortie du mode de conversion
+                if demander_pour_continuer() == False:
+                    break
+            else:
+                print("ERREUR : Veuillez saisir 1 ou 2 ou 3 pour sortir.")
+
+    # sortie du programme
     print("Fin du programme")
 
 # endregion
